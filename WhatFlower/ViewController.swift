52ce5c9b576574b72e682c9fa8dbd9fc5028a12a
Juan Mejia <32790229+mejiajuan50@users.noreply.github.com>
2018-11-08 15:59:50 -0800

    Added a detection method that classifies flowers using the mlmodel.

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -24,11 +26,52 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
-        let userPickedImage = info[UIImagePickerController.InfoKey.editedImage]
-        
-        imageView.image = userPickedImage as? UIImage
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            
+            guard let convertedCIImage = CIImage(image: userPickedImage) else {
+                
+                fatalError("cannot convert to CIImage.")
+            }
+            
+            detect(image: convertedCIImage)
+            
+            imageView.image = userPickedImage
+            
+        }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        //Create a vision container
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            
+            fatalError("Cannot import model")
+        }
+        
+        // Create a request
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            let classification = request.results?.first as? VNClassificationObservation
+            
+            self.navigationItem.title = classification?.identifier.capitalized
+            
+        }
+        
+        // Create a handler
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        
+        catch {
+            print(error)
+        }
+
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
